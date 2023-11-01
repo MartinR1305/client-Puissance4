2f61b2d52951d81d42e004bf5633ce0b99b08e60
Martin <Martin@DESKTOP-N61LV65>
2023-11-01 23:12:28 +0100

    tcp

--- src/controller/ClientController.java
+++ src/controller/ClientController.java
@@ -1,38 +1,84 @@
 package controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class ClientController{
-	
-	//private ClientTCP clientTCP;
-	
+public class ClientController implements Initializable{
+
+	private ClientTCP clientTCP;
+	private static boolean isConnected;
+
 	@FXML
-	private Label client, labelIPAddress,  labelPort,  labelIsConnected, valueIsConnected;
-	
+	private Label client, labelIPAddress, labelPort, state, valueIsConnected;
+
 	@FXML
 	Button connect, send;
-	
+
 	@FXML
-	TextField valueIPAddress,valuePort, message;
+	TextField valueIPAddress, valuePort, message;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+//		setClientTCP(clientTCP);
+//		valueIPAddress.setText(clientTCP.getIP());
+//		valuePort.setText(String.valueOf(clientTCP.getPort()));
+		
+		valueIsConnected.setText("Disconnected");
+		this.updateState();		
+	}
 
 	/**
 	 * Setter of clientTCP -> use for associating emulator and ClientTCP
+	 * 
 	 * @param clientTCP
 	 */
-    public void setClientTCP(ClientTCP clientTCP) {
-        //this.clientTCP = clientTCP;
-    }
-
+	public void setClientTCP(ClientTCP clientTCP) {
+		this.clientTCP = clientTCP;
+	}
 
 	/**
-	 * Start the Thread of Hour
+	 * Method that allows to actualize the state of the connection between the client and the server
+	 * 
+	 * @param state
 	 */
-	@FXML
-	private void initialize() {		
-
+	public void actualizeState(String state) {
+		if (state.equals("Connected")) {
+			isConnected = true;
+		}
 		
+		else if (state.equals("Disconnected")){
+			isConnected = false;
+		}
+	}
+	
+	 /**
+	 * Method that allows to update the state of the connection between the client and the server
+	 */
+	public void updateState() {
+	    Thread stateUpdateThread = new Thread(() -> {
+	        while (true) {
+	            try {
+	                Thread.sleep(500);
+	                Platform.runLater(() -> {
+	                    if (isConnected) {
+	                        valueIsConnected.setText("Connected");
+	                    } else {
+	                        valueIsConnected.setText("Disconnected");
+	                    }
+	                });
+	            } catch (InterruptedException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    });
+	    stateUpdateThread.setDaemon(true);
+	    stateUpdateThread.start();
 	}
 }
--- src/controller/ClientTCP.java
+++ src/controller/ClientTCP.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.SwingUtilities;
 
@@ -13,17 +15,17 @@ public class ClientTCP implements AutoCloseable {
 	private Socket clientSocket;
 	private PrintWriter writer;
 	private BufferedReader reader;
-	
+
 	private ClientController clientController;
-	
+
 	private boolean isConnectedToServer;
 	private boolean isClientOpened;
 	private boolean isReconnectedToServerInProgress;
-	
+
 	private int port;
 	private String IP;
 	private int numClient;
-	
+
 	/**
 	 * Default Constructor for a ClientTCP
 	 * 
@@ -36,193 +38,228 @@ public class ClientTCP implements AutoCloseable {
 		this.isConnectedToServer = false;
 		this.isClientOpened = true;
 		this.isReconnectedToServerInProgress = false;
-		
+
 		this.numClient = -1;
-		
+
 		this.port = port;
 		this.IP = IP;
 		this.clientController = clientController;
 	}
 	
 	/**
-     * Method of launching the connection with the server
-     * @throws IOException
-     */
-    public void connectToServer() throws IOException {
-        try {
-            //For close reconnect () when program is closing
-            if (isClientOpened) {
-                
-                // Initialization of communication 
-                this.clientSocket = new Socket(IP, port);
-    
-                this.writer = new PrintWriter(this.clientSocket.getOutputStream(), true);
-                this.reader = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
-    
-                isConnectedToServer = true;
-    
-                System.out.println("Connected to the server !");
-                
-                /* Get employee ID and send scores serialized */
-                this.writer.println(""); 
-                
-                /* Get Message of server */
-                while (isConnectedToServer && isClientOpened)
-                {
-                    receiveMessage();
-                }
-            }
-        } catch (IOException IOE) {
-            /* If the server aren't open*/
-            if (this.clientSocket == null) {
-                if (isClientOpened) {
-                    System.err.println("Server not open !");
-                    reconnect();
-                }
-            } else {
-                IOE.printStackTrace();
-            }
-        }
-    }
-    
-    /**
-     * Use to retry a connection with the server
-     */
-    private void reconnect() {
-        
-        //if the port is being modified
-        if (isReconnectedToServerInProgress) {
-            return;
-        }
-        
-        //Start the Thread to take a break
-        Thread waitThread = new Thread(new Runnable() {
-            public void run() {
-                try {
-                    Thread.sleep(5000); //5 second of break
-                    SwingUtilities.invokeLater(new Runnable() {
-                        public void run() {
-                            if (!isConnectedToServer || isClientOpened) {
-                                try {
-                                    connectToServer(); //Retry the connection
-                                } catch (IOException IOE) {
-                                    IOE.printStackTrace();
-                                }
-                            }
-                        }
-                    });
-                } catch (InterruptedException IE) {
-                    IE.printStackTrace();
-                }
-            }
-        });
-        
-        //Starting the Thread
-        waitThread.start();
-    }
-    
-    /**
-     * Method use to close socket, Reader and Writer
-     */
-    public void disconnect() {
-        try {
-            if (this.clientSocket != null) {
-                //Reader
-                if (this.reader != null) {
-                    this.reader.close();
-                }
-                
-                //Writer
-                if (this.writer != null) {
-                    this.writer.close();
-                }
+	 * Getter for the client's number
+	 * 
+	 * @return
+	 */
+	public int getNumClient() {
+		return this.numClient;
+	}
+	
+	/**
+	 * Getter for the client's IP
+	 * 
+	 * @return
+	 */
+	public String getIP() {
+		return this.IP;
+	}
+	
+	/**
+	 * Getter for the client's IP
+	 * 
+	 * @return
+	 */
+	public int getPort() {
+		return this.port;
+	}
 
-                System.out.println("Socket Closed");
-                this.clientSocket.close();
-            }
-            
-            //If the server is close before the Scoring Machine
-            this.clientSocket = null;
+	/**
+	 * Method of launching the connection with the server
+	 * 
+	 * @throws IOException
+	 */
+	public void connectToServer() throws IOException {
+		try {
+			// For close reconnect () when program is closing
+			if (isClientOpened) {
 
-            isConnectedToServer = false;
+				// Initialization of communication
+				this.clientSocket = new Socket(IP, port);
 
-        } catch (IOException IOE) {
-            System.err.println("Error disconnecting from the server: " + IOE.getMessage());
-        }
-    }
-    
-    /**
-     * Method the receive Messages from the Server and Update FXML page
-     */
-    public void receiveMessage() {
-        String finalMessage = "";
-        try {
-            //Read Message
-            finalMessage = this.reader.readLine();
-    
-            //If the connection have been stopped
-            if (finalMessage.equals("STOP")) {
-                System.out.println("Connection to the server lost !");
-                
-                //Update status in FXML page
-                String [] disconnect = {"disconnected"};
-                //clientController.PrepMiseAJour (disconnect);
-                
-                //Closure of socket / in / out
-                disconnect();
-                
-                //If the program are stopping -> don't retry the connection
-                if (isClientOpened) {
-                    reconnect();
-                }
-    
-            } else {
-                System.out.println("all good");
-            }
-        }
-        catch (IOException IOE) {
-            IOE.printStackTrace();
-            disconnect();
-        }
-    }
-    
-    /**
-     * Using to change Port / IP of the connection to Server
-     * @param IP : IP for connect to the server
-     * @param port : Port for connect to the server
-     */
-    public void sendIP_Port (String IP, String Port) {
-        
-        System.out.println ("Connection to : " + IP + " - " + Port);
-        
-        this.IP = IP;
-        port = Integer.parseInt(Port);
-        
-        isReconnectedToServerInProgress = true;
-        
-        //Stop A potential Thread
-        if (isConnectedToServer) {
-            this.writer.println ("STOP");
-        }
-        
-        //Update status in FXML page -> potential new server
-        String [] disconnect = {"disconnectedBis"};
-        //clientController.PrepMiseAJour (disconnect);
-        
-        //Reconnect with new informations
-        reconnect ();
-        
-        isReconnectedToServerInProgress = false;
-    }
-    
-    /**
-     * Method use to send numColumn to Server
-     * @param Saisie_ID
-     * @param round_hour
-     */
-    public void sendMessage(int numColumn) {
+				this.writer = new PrintWriter(this.clientSocket.getOutputStream(), true);
+				this.reader = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
 
-    }
+				isConnectedToServer = true;
+
+				System.out.println("Connected to the server !");
+				clientController.actualizeState("Connected");
+
+				/* Get Message of server */
+				while (isConnectedToServer && isClientOpened) {
+					receiveMessage();
+				}
+			}
+		} catch (IOException IOE) {
+			/* If the server aren't open */
+			if (this.clientSocket == null) {
+				if (isClientOpened) {
+					System.err.println("Server not open !");
+					
+					reconnect();
+				}
+			} else {
+				IOE.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Use to retry a connection with the server
+	 */
+	private void reconnect() {
+
+		// if the port is being modified
+		if (isReconnectedToServerInProgress) {
+			return;
+		}
+
+		// Start the Thread to take a break
+		Thread waitThread = new Thread(new Runnable() {
+			public void run() {
+				try {
+					Thread.sleep(5000); // 5 second of break
+					SwingUtilities.invokeLater(new Runnable() {
+						public void run() {
+							if (!isConnectedToServer || isClientOpened) {
+								try {
+									connectToServer(); // Retry the connection
+								} catch (IOException IOE) {
+									IOE.printStackTrace();
+								}
+							}
+						}
+					});
+				} catch (InterruptedException IE) {
+					IE.printStackTrace();
+				}
+			}
+		});
+
+		// Starting the Thread
+		waitThread.start();
+	}
+
+	/**
+	 * Method use to close socket, Reader and Writer
+	 */
+	public void disconnect() {
+		try {
+			if (this.clientSocket != null) {
+				// Reader
+				if (this.reader != null) {
+					this.reader.close();
+				}
+
+				// Writer
+				if (this.writer != null) {
+					this.writer.close();
+				}
+
+				System.out.println("Socket Closed");
+				this.clientSocket.close();
+				clientController.actualizeState("Disconnected");
+			}
+
+			// If the server is close before the Scoring Machine
+			this.clientSocket = null;
+
+			isConnectedToServer = false;
+
+
+		} catch (IOException IOE) {
+			System.err.println("Error disconnecting from the server: " + IOE.getMessage());
+		}
+	}
+
+	/**
+	 * Method the receive Messages from the Server and Update FXML page
+	 */
+	public void receiveMessage() {
+		String finalMessage = "";
+		try {
+			// Read Message
+			finalMessage = this.reader.readLine();
+
+			// If the connection have been stopped
+			if (finalMessage.equals("STOP")) {
+				System.out.println("Connection to the server lost !");
+
+				// Update status in FXML page
+				clientController.actualizeState("Disconnected");
+
+				// Closure of socket / in / out
+				disconnect();
+
+				// If the program are stopping -> don't retry the connection
+				if (isClientOpened) {
+					reconnect();
+				}
+
+			}
+
+			Pattern pattern = Pattern.compile("N(\\d+)");
+			Matcher matcher = pattern.matcher(finalMessage);
+
+			// Message for client's number assignment
+			if (matcher.find()) {
+	            String integerPart = matcher.group(1); 
+	            numClient = Integer.parseInt(integerPart) - 1; 
+				
+			}
+		} catch (IOException IOE) {
+			IOE.printStackTrace();
+			disconnect();
+		}
+	}
+
+	/**
+	 * Using to change Port / IP of the connection to Server
+	 * 
+	 * @param IP   : IP for connect to the server
+	 * @param port : Port for connect to the server
+	 */
+	public void sendIP_Port(String IP, String Port) {
+
+		System.out.println("Connection to : " + IP + " - " + Port);
+
+		this.IP = IP;
+		port = Integer.parseInt(Port);
+
+		isReconnectedToServerInProgress = true;
+
+		// Stop A potential Thread
+		if (isConnectedToServer) {
+			this.writer.println("STOP");
+		}
+
+		// Update status in FXML page -> potential new server
+		clientController.actualizeState("Disconnected");
+
+		// Reconnect with new informations
+		reconnect();
+
+		isReconnectedToServerInProgress = false;
+	}
+
+	/**
+	 * Method use to send numColumn to Server
+	 * 
+	 * @param Saisie_ID
+	 * @param round_hour
+	 */
+	public void sendMessage(int numColumn) {
+
+	}
 
 	/**
 	 * Method that allows to close the client
@@ -230,11 +267,11 @@ public class ClientTCP implements AutoCloseable {
 	@Override
 	public void close() throws Exception {
 		isClientOpened = false;
-		
-		if(writer != null) {
+
+		if (writer != null) {
 			writer.println("STOP");
 		}
-		
+
 		System.out.println("Client stopped");
 	}
 
